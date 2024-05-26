8c2014775ba8100435614be2e2a97824b410632e
blackrockxi37 <agentsmith226@gmail.com>
2024-05-26 15:13:57 +0300

    fix

--- main.py
+++ main.py
@@ -108,7 +108,6 @@ def link_generator():
     keyboard = types.InlineKeyboardMarkup()
     for i in ls:
         calldata = i.split()[0]
-        calldata = i.split()[0]
         keyboard.add(types.InlineKeyboardButton(text = i, callback_data=f'sendme {calldata}'))
     ls = '\n'.join(ls)
     bot.send_message(chat_id=rockxi, text='Выберите файл:', reply_markup=keyboard)
